b9f0582d208fde7ef4ae2f33aa4f0febfc677608
ofunc <ofunc@outlook.com>
2020-01-28 15:44:18 +0800

    Add LP test.

---
+++ lp_test.go
@@ -0,0 +1,65 @@
+package ga_test
+
+import (
+	"math"
+	"math/rand"
+	"testing"
+
+	"github.com/ofunc/ga"
+)
+
+type LP [2]float64
+
+func (x LP) Fitness() float64 {
+	s, f := 0.0, 72*x[0]+64*x[1]
+	if d := (x[0] + x[1]) / 50; d > 1 {
+		s += d - 1
+	}
+	if d := (12*x[0] + 8*x[1]) / 480; d > 1 {
+		s += d - 1
+	}
+	if d := (3 * x[0]) / 100; d > 1 {
+		s += d - 1
+	}
+	if s > 0 {
+		return -s
+	} else {
+		return f
+	}
+}
+
+func (x LP) Mutate() ga.Entity {
+	return genLP()
+}
+
+func (x LP) Crossover(e ga.Entity, w float64) ga.Entity {
+	y := e.(LP)
+	x[0] = math.Round(w*x[0] + (1-w)*y[0])
+	x[1] = math.Round(w*x[1] + (1-w)*y[1])
+	return x
+}
+
+func TestLP(t *testing.T) {
+	m := ga.New(100, genLP)
+	e, f, ok := m.Evolve(32, 100000)
+	if e != m.Elite() {
+		t.FailNow()
+	}
+	if math.Abs(f-e.Fitness()) > 1e-10 {
+		t.FailNow()
+	}
+	if math.Abs(f-m.Fitness()) > 1e-10 {
+		t.FailNow()
+	}
+	if !ok {
+		t.FailNow()
+	}
+
+	if f < 3360-100 {
+		t.Fatal("fitness(3360):", f)
+	}
+}
+
+func genLP() ga.Entity {
+	return LP{100 * rand.Float64(), 100 * rand.Float64()}
+}
